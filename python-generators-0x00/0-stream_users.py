06179c354dfef2fc0847d837fd73041594cab20b
abuaminuu <43301447+abuaminuu@users.noreply.github.com>
2025-10-31 22:02:10 +0100

    Create 0-stream_users.py

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,14 @@
+# py
+"""
+write a function that uses a generator to fetch rows one by one 
+from the user_data table. You must use the Yield python generator
+Prototype: def stream_users()
+Your function should have no more than 1 loop
+""" 
+
+from . import seed
+
+def stream_users():
+    # connect to the database
+    connection = seed.connect_prodev()
+    # pass
