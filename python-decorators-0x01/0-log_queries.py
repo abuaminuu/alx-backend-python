b23b9c7b19f084049c45b354ab0532594b7a5bd1
abuaminuu <43301447+abuaminuu@users.noreply.github.com>
2025-11-04 14:39:58 +0100

    Create 0-log_queries.py

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,25 @@
+import sqlite3
+import functools
+
+#### decorator to log SQL queries before executing them
+
+def log_queries():
+  def decorator(func):
+    def wrapper(*args, **kwargs):
+      result = func(*args, **kwargs)
+      return result
+    return wrapper
+  return decorator
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
