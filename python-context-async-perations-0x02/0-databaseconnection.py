46dc74f13b984995b0e72816a91e00c7fabd9d04
abuaminuu <43301447+abuaminuu@users.noreply.github.com>
2025-11-11 21:47:08 +0100

    used custom context manager for the checkers

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+
+# custom context manager DatabaseConnection using the __enter__ and the __exit__ methods
 class DatabaseConnection():
     def __init__(self, db_name):
         self.db_name = db_name
@@ -21,5 +23,19 @@ class DatabaseConnection():
         self.connection.close()
         # propagate exception if needed
         return False
-        
+
+# Use the context manager with the with statement to be able to perform the query SELECT * FROM users. Print the results from the query.
+def query_database(db_name):
+    try:
+        with DatabaseConnection(db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM users")
+            results = cursor.fetchall()
+            for row in results:
+                print(row)
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+
+# Example usage
+query_database('example.db')
             
