253e9bb227bead0bb078422b88968532e2cd742c
abuaminuu <i.abuaminu@gmail.com>
2025-11-15 21:26:03 +0100

    edit models 2

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,22 +1,15 @@
-from django.db import models
 import uuid
-from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
-# Create your models here.
 class User(models.Model):
     user_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True
     )
-
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-
     email = models.EmailField(unique=True)
-
-    # Checker wants the field named EXACTLY "password"
     password = models.CharField(max_length=255)
-
     phone_number = models.CharField(max_length=20, null=True, blank=True)
 
     ROLE_CHOICES = [
@@ -24,7 +17,6 @@ class User(models.Model):
         ("host", "Host"),
         ("admin", "Admin"),
     ]
-
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -39,43 +31,35 @@ class User(models.Model):
 
 
 class Conversation(models.Model):
-    id = models.UUIDField(
+    conversation_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True
     )
-
     participants = models.ManyToManyField(
         User,
         related_name="conversations"
     )
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversation {self.id}"
+        return f"Conversation {self.conversation_id}"
+
 
 class Message(models.Model):
-    id = models.UUIDField(
+    message_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True
     )
-
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="messages_sent"
+    )
     conversation = models.ForeignKey(
         Conversation,
         on_delete=models.CASCADE,
         related_name="messages"
     )
-
-    sender = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="sent_messages"
-    )
-
     message_body = models.TextField()
-
     sent_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ["sent_at"]
-
     def __str__(self):
-        return f"Message from {self.sender.username}"
+        return f"Message {self.message_id}"
