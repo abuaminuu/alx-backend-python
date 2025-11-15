cccbacda8155d47e3a6b28b067357a1499e33bb5
abuaminuu <i.abuaminu@gmail.com>
2025-11-15 21:03:19 +0100

    remove git syntax

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -35,20 +35,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-<<<<<<< HEAD
-    'chats',
-=======
->>>>>>> init commit
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-=======
     'chats',
->>>>>>> init commit
     'rest_framework',
 ]
 
