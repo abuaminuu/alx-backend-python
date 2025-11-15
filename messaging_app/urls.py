0b6b3f870e9a9dba28e06b06e103b6e313249c14
abuaminuu <i.abuaminu@gmail.com>
2025-11-15 22:33:10 +0100

    modify urls and views

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """messaging_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +19,12 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+=======
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("chats.urls")),
+>>>>>>> added urls file
 ]
