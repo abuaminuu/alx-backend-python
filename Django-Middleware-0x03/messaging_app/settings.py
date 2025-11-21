2c065caf9b37f19ed4cd3a4c8394dd126bc2d5e2
abuaminuu <i.abuaminu@gmail.com>
2025-11-21 14:36:52 +0100

    added settings, middleware and requests logs

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -115,7 +115,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "chats.middleware.RequestLoggingMiddleware"
+    # Our Custom middleware
+    'messaging_app.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
