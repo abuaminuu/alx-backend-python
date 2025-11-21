3378b625eafb5d221c76a8749fcd893e4e6eed65
abuaminuu <i.abuaminu@gmail.com>
2025-11-21 14:18:35 +0100

    updated settings.py

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -115,7 +115,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'messaging_app.middleware.RequestLoggingMiddleware',
+    'messaging_app.chats.middleware.RequestLoggingMiddleware',
+    'messaging_app.chatsx.middleware.RequestLoggingMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
