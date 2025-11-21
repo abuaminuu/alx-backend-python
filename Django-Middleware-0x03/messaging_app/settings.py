345e7cb5a004245e7df9895434c497ed40432ed7
abuaminuu <i.abuaminu@gmail.com>
2025-11-21 14:22:18 +0100

    added logs

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -115,8 +115,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'chats.middleware.RequestLoggingMiddleware',
-
+    "chats.middleware.RequestLoggingMiddleware"
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
